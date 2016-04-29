456ec056abb1f8d3978811f95e22e260c7b6ee8b
awat17 <ali.aliyan1@gmail.com>
2016-04-29 22:57:58 +0430

    make it better by saeed gnu

--- todo.py
+++ todo.py
@@ -1,12 +1,12 @@
 print("hello ")
 list = []
-pr = 1
 while 1:
     ToDoinput = raw_input()
     if (ToDoinput =="exit"):
         break
     priority, name, date, state = ToDoinput.split()
     list.insert(int(priority),[int (priority) , name , date, state])
+    pr = 1
     for item in list:
         print ("{} ) ToDo: {}   dead line: {} satatus: {} ".format(pr , list[pr-1][1], list[pr-1][2], list[pr-1][3]))
         pr+= 1
