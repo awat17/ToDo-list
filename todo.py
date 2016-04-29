6fd8bc17326c41d745353115614c5a2b6ce91ebb
awat17 <ali.aliyan1@gmail.com>
2016-04-29 21:16:20 +0430

    second commit :)

--- todo.py
+++ todo.py
@@ -1,8 +1,27 @@
 print("hello ")
 list = []
-i ,tmp = str(raw_input()).split()
-list.insert(int(i),tmp)
-i = 1
-for item in list:
-    print ("%d   %s " % (i , list[i-1]))
-    i = i+1
+class ToDo:
+    ToDoCount = 0
+
+    def __init__(self, priority, name, date, state):
+        self.priority = priority
+        self.name = name
+        self.date = date
+        self.state = state
+        ToDo.ToDoCount +=1
+
+    def new_item (self, priority, name, date, state):
+            self.insert(int (self.priority), self.name, self.date, self.state )
+
+    def count (self):
+        print "all ToDo is %d" %ToDo.ToDoCount
+
+    def displayToDo(self):
+      print "Name : ", self.name,  ", date: ", self.date
+pr = 1
+while 1:
+    priority, name, date, state = raw_input().split()
+    list.insert(int(priority),ToDo(priority, name, date, state))
+    for item in list:
+        print ("%d   %s " % (pr , list[pr-1]))
+        pr = pr +1
